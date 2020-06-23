d5de05422a36252b11ff388eb18109c96835b5c0
Tabatha Flores <tabatha.flores18@gmail.com>
2020-06-23 11:45:06 -0600

    Create Spring Boot Project for REST API

---
+++ StockOverflow/src/main/java/com/skilldistillery/stockoverflow/StockOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.stockoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOverflowApplication.class, args);
+	}
+
+}
