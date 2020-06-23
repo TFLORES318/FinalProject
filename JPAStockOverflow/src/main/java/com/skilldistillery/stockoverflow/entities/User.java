095802e6ac3c09e89edef10346bae5f4138f49cc
Tabatha Flores <tabatha.flores18@gmail.com>
2020-06-23 11:39:05 -0600

    Annotate User Entity

--- JPAStockOverflow/src/main/java/com/skilldistillery/stockoverflow/entities/User.java
+++ JPAStockOverflow/src/main/java/com/skilldistillery/stockoverflow/entities/User.java
@@ -1,5 +1,46 @@
 package com.skilldistillery.stockoverflow.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+
+	
+	
+	
+	
+	
+	public User() {
+	}
+
+	public User(int id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
