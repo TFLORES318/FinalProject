6b745a63ac93ba670871e6e98dd5b75f3e6f3180
arpapp <trpapp@gmail.com>
2020-06-24 09:35:19 -0600

    comment rating repo test passed

--- StockOverflow/src/main/java/com/skilldistillery/stockoverflow/repositories/CommentRatingRepository.java
+++ StockOverflow/src/main/java/com/skilldistillery/stockoverflow/repositories/CommentRatingRepository.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.stockoverflow.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.skilldistillery.stockoverflow.entities.CommentRating;
+import com.skilldistillery.stockoverflow.entities.CommentRatingId;
 
-public interface CommentRatingRepository extends JpaRepository<CommentRating, Integer> {
-
+@Repository
+public interface CommentRatingRepository extends JpaRepository<CommentRating, CommentRatingId> {
 }
