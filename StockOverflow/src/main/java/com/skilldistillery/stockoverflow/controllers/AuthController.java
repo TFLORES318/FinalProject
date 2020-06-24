6d7f0097383731cd144f94dfb075f013983bbc26
Rich Wasek <rideburtonrw@gmail.com>
2020-06-24 12:40:22 -0600

    Updated Security config/Auth Controller register/login routes and tested Register in Postman and it was successful

---
+++ StockOverflow/src/main/java/com/skilldistillery/stockoverflow/controllers/AuthController.java
@@ -0,0 +1,44 @@
+package com.skilldistillery.stockoverflow.controllers;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.stockoverflow.entities.User;
+import com.skilldistillery.stockoverflow.services.AuthService;
+
+@RestController
+@CrossOrigin({"*", "http://localhost:4210"})
+public class AuthController {
+
+	@Autowired
+	private AuthService authService;
+	
+	@PostMapping("/register")
+	public User register(
+			@RequestBody User user,
+			HttpServletResponse res
+	) {
+		if (user == null) {
+			res.setStatus(400);
+		}
+		user = authService.register(user);
+		return user;
+	}
+	
+	@GetMapping("/login")
+	// principal checks the username/pw - if they are valid depending on encoder
+	public Principal authenticate(Principal principal) {
+	    return principal;
+	}
+	
+	
+	
+}
