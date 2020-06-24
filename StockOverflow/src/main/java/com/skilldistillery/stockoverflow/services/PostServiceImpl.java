fcd9302b1528f2d9773cb7b8fcde2c9cad3e8f40
Tabatha Flores <tabatha.flores18@gmail.com>
2020-06-24 12:46:59 -0600

    post service interface and impl - added methods to repository for post

---
+++ StockOverflow/src/main/java/com/skilldistillery/stockoverflow/services/PostServiceImpl.java
@@ -0,0 +1,74 @@
+package com.skilldistillery.stockoverflow.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.stockoverflow.entities.Post;
+import com.skilldistillery.stockoverflow.entities.User;
+import com.skilldistillery.stockoverflow.repositories.PostRepository;
+import com.skilldistillery.stockoverflow.repositories.UserRepository;
+
+@Service
+public class PostServiceImpl implements PostService {
+	
+	@Autowired
+	private PostRepository postRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public List<Post> index() {
+		return postRepo.findAll();
+	}
+
+	@Override
+	public List<Post> userPosts(String username) {
+		return postRepo.findByUserUsername(username);
+	}
+
+	@Override
+	public Post show(String username, int postId) {
+		Post post = postRepo.findByIdAndUserUsername(postId, username);
+		return post;
+	}
+
+	@Override
+	public Post create(String username, Post post) {
+		User user = userRepo.findByUsername(username);
+		if (user != null) {
+			post.setUser(user);
+			postRepo.saveAndFlush(post);
+			return post;
+		}
+		return null;
+	}
+
+	@Override
+	public Post update(String username, int postId, Post post) {
+		Post updatedPost = postRepo.findByIdAndUserUsername(postId, username);
+		if (updatedPost != null) {
+			updatedPost.setTitle(post.getTitle());
+			updatedPost.setDescription(post.getDescription());
+			postRepo.saveAndFlush(updatedPost);
+			return updatedPost;
+			
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean disable(String username, int postId) {
+		boolean deletedDisable = false;
+		Post post = postRepo.findByIdAndUserUsername(postId, username);
+		if (post != null) {
+			post.setEnabled(false);
+			postRepo.saveAndFlush(post);
+			deletedDisable = true;
+		}
+		return deletedDisable;
+	}
+
+}
