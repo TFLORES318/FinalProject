3deebb0ac85146c7888c21a3317dd3b2c3fbb09d
arpapp <trpapp@gmail.com>
2020-06-24 12:26:54 -0600

    db change, comment serv

--- DB/stockoverflowdb.sql
+++ DB/stockoverflowdb.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(100) NOT NULL,
   `password` VARCHAR(200) NOT NULL,
-  `flair` VARCHAR(45) NOT NULL,
+  `flair` VARCHAR(45) NULL,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NULL,
