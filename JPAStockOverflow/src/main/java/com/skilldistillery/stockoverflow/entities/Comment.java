e809f4230a04a0a10441964eae0e6a456ba04536
Tabatha Flores <tabatha.flores18@gmail.com>
2020-06-23 18:17:18 -0600

    comment, user with enum, webinar, comment test

---
+++ JPAStockOverflow/src/main/java/com/skilldistillery/stockoverflow/entities/Comment.java
@@ -0,0 +1,69 @@
+package com.skilldistillery.stockoverflow.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@CreationTimestamp
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+
+	public Comment() {}
+
+	public Comment(int id, String content, LocalDateTime createDate) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.createDate = createDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentEntity [id=" + id + ", content=" + content + ", createDate=" + createDate + "]";
+	}
+	
+	
+	
+	
+	
+
+}
