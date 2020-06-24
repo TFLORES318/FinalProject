31c92054c052cda1e052d9c26abcefba80b59b87
arpapp <trpapp@gmail.com>
2020-06-24 09:47:22 -0600

    webinar rating repo passed test

--- StockOverflow/src/main/java/com/skilldistillery/stockoverflow/repositories/WebinarRatingRepository.java
+++ StockOverflow/src/main/java/com/skilldistillery/stockoverflow/repositories/WebinarRatingRepository.java
@@ -3,7 +3,8 @@ package com.skilldistillery.stockoverflow.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.stockoverflow.entities.WebinarRating;
+import com.skilldistillery.stockoverflow.entities.WebinarRatingId;
 
-public interface WebinarRatingRepository extends JpaRepository<WebinarRating, Integer> {
+public interface WebinarRatingRepository extends JpaRepository<WebinarRating, WebinarRatingId> {
 
 }
