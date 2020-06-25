061ef80270d198438903ab26fd5e1f7496a1712c
arpapp <trpapp@gmail.com>
2020-06-25 16:25:43 -0600

    comment serv changes, post serv, forum component

--- ngStockOverflow/src/app/components/forum/forum.component.ts
+++ ngStockOverflow/src/app/components/forum/forum.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+import { Router } from '@angular/router';
+import { Post } from 'src/app/models/post';
+import { Comment } from 'src/app/models/comment';
+import { AuthService } from 'src/app/services/auth.service';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-forum',
@@ -7,9 +13,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumComponent implements OnInit {
 
-  constructor() { }
+  allPosts: Post[] = [];
+
+  newPost = new Post();
+
+  postComments = [];
+
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private auth: AuthService,
+    private commentService: CommentService
+  ) { }
 
   ngOnInit(): void {
+    this.loadAllPosts();
+  }
+
+  loadAllPosts(){
+    this.postService.index().subscribe(
+      data => {
+        this.allPosts = data;
+        this.allPosts.forEach(value => {
+          this.loadComments(value);
+        })
+      },
+      err => {
+        console.log('error in load all posts form component');
+
+      }
+    )
+  }
+
+  createPost(post: Post){
+    this.postService.create(post).subscribe(
+      data => {
+        console.log('post creation success');
+        this.loadAllPosts();
+      },
+      err => {
+        console.error('post creation failure');
+      }
+    )
+  }
+
+  updatePost(post: Post){
+    this.postService.update(post).subscribe(
+      data => {
+        console.log('post update success');
+        this.loadAllPosts();
+      },
+      err => {
+        console.error('post update failure');
+      }
+    )
+  }
+
+  deletePost(postId: number){
+    this.postService.disable(postId).subscribe(
+      data => {
+        console.log('post delete success');
+        this.loadAllPosts();
+      },
+      err => {
+        console.error('post deletion failure');
+
+      }
+    )
+  }
+
+  loadComments(post: Post){
+    this.commentService.index(post.id).subscribe(
+      data => {
+        post.comments = data;
+        console.log('load comment success in forum comp ts');
+        console.log(this.postComments);
+      },
+      err => {
+        console.error('load comments in forum comp ts error');
+
+      }
+    )
   }
 
 }
--- ngStockOverflow/src/app/services/post.service.ts
+++ ngStockOverflow/src/app/services/post.service.ts
@@ -1,9 +1,117 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Post } from '../models/post';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8090/';
+  private url = this.baseUrl + 'api/posts';
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+
+  index(){
+    return this.http.get<Post[]>(this.url)
+    .pipe(
+      catchError((err: any) => {
+        console.log('post service index method is broken');
+        return throwError('post service index method is broken');
+      })
+    )
+  }
+
+  allPostsForUser(userId: number){
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    }
+    return this.http.get<Post[]>(this.baseUrl + 'api/users/posts')
+    .pipe(
+      catchError((err: any) => {
+        console.log('error in all posts for user post service');
+        return throwError(' error in all posts for user post service')
+      })
+    )
+  }
+
+  getPostById(postId: number){
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    }
+    return this.http.get<Post>(this.url + '/' + postId)
+    .pipe(
+      catchError((err: any) => {
+        console.log('error in get post by id post service');
+        return throwError('error in get post by id post service')
+      })
+    )
+  }
+
+  create(newPost: Post){
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    }
+    return this.http.post<Post>(this.url, newPost)
+    .pipe(
+      catchError((err: any) => {
+        console.log('post service create is broken');
+        return throwError('post service create is broken');
+      })
+    )
+  }
+
+  update(post: Post){
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    }
+    return this.http.put<Post>(this.url + '/' + post.id, post)
+    .pipe(
+      catchError((err: any) => {
+        console.log('post service update is broken');
+        return throwError('post service update is broken');
+      })
+    )
+  }
+
+  disable(postId: number){
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    }
+    return this.http.put(this.baseUrl + '/disable/' + postId, postId)
+    .pipe(
+      catchError((err: any) => {
+        console.log('error in post serv disable');
+        return throwError('error in post service disable')
+      })
+    )
+  }
+
+
 }
