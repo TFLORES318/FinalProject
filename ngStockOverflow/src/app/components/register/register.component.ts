04a7e5607d26e2ae974e66e5b10164fd691b3a39
Rich Wasek <rideburtonrw@gmail.com>
2020-06-25 14:13:04 -0600

    stubbed out login/register components and auth service

---
+++ ngStockOverflow/src/app/components/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  newUser = new User();
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  register(newUserForm: NgForm) {
+    const user: User = newUserForm.value;
+    this.auth.register(user).subscribe(
+      data => {
+        console.log('RegisterComponent.register(): user registered.');
+        this.auth.login(user.username, user.password).subscribe(
+          next => {
+            console.log('RegisterComponent.register(): user logged in, routing to /home.');
+            this.router.navigateByUrl('/home'); // or profile
+          },
+          error => {
+            console.error('RegisterComponent.register(): error logging in.');
+          }
+        );
+      },
+      err => {
+        console.error('RegisterComponent.register(): error registering.');
+        console.error(err);
+      }
+    );
+  }
+
+}
