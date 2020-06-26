634336a7da4bffb7c8b348cc76001f76358cf14a
arpapp <trpapp@gmail.com>
2020-06-25 18:32:52 -0600

    user can add stocks to portfolio

--- ngStockOverflow/src/app/components/portfolio/portfolio.component.ts
+++ ngStockOverflow/src/app/components/portfolio/portfolio.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 import { StockService } from 'src/app/services/stock.service';
 import { Stock } from 'src/app/models/stock';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-portfolio',
@@ -33,6 +34,20 @@ export class PortfolioComponent implements OnInit {
 
       }
     )
+  }
+
+  addNewStock(form: NgForm){
+    const newStock: Stock = form.value;
+    this.stockService.createStock(newStock).subscribe(
+      data => {
+        console.log('stock creation success');
+        this.loadUsersStocks();
+      },
+      err => {
+        console.error('error creating stock');
+      }
+    )
+    form.reset();
   }
 
 }
