6d5dbbde8db5aa47ae8ff088d6b5d1a3c27a3fa8
arpapp <trpapp@gmail.com>
2020-06-27 18:46:45 -0600

    other profile view fleshed out

--- ngStockOverflow/src/app/app-routing.module.ts
+++ ngStockOverflow/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AdminComponent } from './components/admin/admin.component';
-
+import { OtherUserProfileComponent} from './components/other-user-profile/other-user-profile.component';
 
 
 const routes: Routes = [
@@ -23,6 +23,7 @@ const routes: Routes = [
   { path: 'forum', component: ForumComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'profile', component: ProfileViewComponent },
+  { path: 'profile/:id', component: OtherUserProfileComponent },
   { path: 'resources', component: ResourcesComponent },
   { path: 'webinars', component: WebinarsComponent },
   { path: 'login', component: LoginComponent },
---
+++ ngStockOverflow/src/app/components/other-user-profile/other-user-profile.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostService } from 'src/app/services/post.service';
+import { User } from 'src/app/models/user';
+import { Post } from 'src/app/models/post';
+import { Webinar } from 'src/app/models/webinar';
+
+@Component({
+  selector: 'app-other-user-profile',
+  templateUrl: './other-user-profile.component.html',
+  styleUrls: ['./other-user-profile.component.css']
+})
+export class OtherUserProfileComponent implements OnInit {
+
+  user = new User();
+
+  userId: number = 0;
+
+  posts: Post[] = [];
+
+  enabledPosts: Post[] = [];
+
+  webinars: Webinar[] = [];
+
+  enabledWebinars: Webinar[] = [];
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private currentRoute: ActivatedRoute,
+    private router: Router,
+    private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.currentRoute.paramMap.subscribe(
+      params => {
+        console.log(params.get('id'))
+        this.userId = parseInt(params.get('id'));
+        console.log(this.userId);
+      }
+    )
+    this.getSpecificUser();
+    this.loadPosts();
+    this.loadWebinars();
+  }
+
+  getSpecificUser() {
+    console.log(this.userId);
+    return this.userService.getAnotherUser(this.userId).subscribe(
+      data => {
+        this.user = data;
+        console.log(this.user);
+      },
+      fail => {
+        console.error('error in profile component - cannot obtain user')
+      }
+    );
+  }
+
+  loadWebinars() {
+    return this.userService.allWebinarsForUser(this.user.id).subscribe(
+      data => {
+        this.webinars = data;
+        for (let i = 0; i < this.webinars.length; i++) {
+          if (this.webinars[i].enabled) {
+            this.enabledWebinars.push(this.webinars[i]);
+          }
+        }
+        console.log('webinars retrieved');
+      },
+      fail => {
+        console.error('could not retrieve user webinars from profile view component');
+      }
+    );
+  }
+
+  loadPosts() {
+    return this.postService.allPostsForUser(this.user.id).subscribe(
+      data => {
+        this.posts = data;
+       for (let index = 0; index < this.posts.length; index++) {
+         if (this.posts[index].enabled) {
+           this.enabledPosts.push(this.posts[index]);
+         }
+
+       }
+        console.log(localStorage.getItem('credentials'));
+      },
+      fail => {
+        console.error('ERRORRRR');
+      }
+    );
+  }
+
+
+}
